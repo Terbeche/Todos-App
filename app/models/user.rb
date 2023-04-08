64618568f6deae959b84caee03b8db3d545cfda5
terbeche <termus96@gmail.com>
2023-04-08 15:15:57 +0100

    Add necessary validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
-
 class User < ApplicationRecord
-  has_many :todos, dependent: :destroy
-end
+    has_many :todos, dependent: :destroy
+
+    validates :email,
+              presence: true,
+              uniqueness: { case_sensitive: false },
+              format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+    passwordless_with :email
+  end
