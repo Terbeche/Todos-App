f7b611b8ae4c527bc9c815c25c2c1cca08172094
terbeche <termus96@gmail.com>
2023-04-08 15:46:45 +0100

    Implement user authentication with email using passwordless

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
-class User < ApplicationRecord
-    has_many :todos, dependent: :destroy
 
-    validates :email,
-              presence: true,
-              uniqueness: { case_sensitive: false },
-              format: { with: URI::MailTo::EMAIL_REGEXP }
-  
-    passwordless_with :email
+class User < ApplicationRecord
+  has_many :todos, dependent: :destroy
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  passwordless_with :email
+
+  def self.fetch_resource_for_passwordless(email)
+    find_or_create_by(email: email)
   end
+end
