d40089a64971f4d4fac2b49f5ce29d1d3c0a61f4
terbeche <termus96@gmail.com>
2023-04-08 15:05:38 +0100

    Fix linter errors using rubocop

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class Todo < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,26 +12,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_140249) do
+ActiveRecord::Schema[7.0].define(version: 20_230_408_140_249) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "todos", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.date "due_date"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_todos_on_user_id"
+  create_table 'todos', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.date 'due_date'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_todos_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
+  create_table 'users', force: :cascade do |t|
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email'
   end
 
-  add_foreign_key "todos", "users"
+  add_foreign_key 'todos', 'users'
 end
