26bd1cd0338c936c6eefce2fad56f9747716762d
terbeche <termus96@gmail.com>
2023-04-08 05:41:43 +0100

    Congigure email authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!, unless: -> { request.path_info =~ /\/users/ }
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    end
+  
+    def after_sign_in_path_for(resource)
+      todos_path
+    end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable,
+
+ devise :omniauthable: [:email]
+
+  has_many :todos, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.email
+  end
+
+  def email_required?
+    false
+  end
 end
