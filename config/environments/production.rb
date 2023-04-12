556e5a07a440f495a9ce67b10e84ec557273118c
terbeche <termus96@gmail.com>
2023-04-12 20:34:38 +0100

    Set gmail as email delivery sercice for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,10 +7,16 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_options = { host: "https://todos-j2ps.onrender.com" }
-
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'https://todos-j2ps.onrender.com',
+    user_name: 'termus96@gmail.com',
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
