54158e6f252261e9a5e507423505fec89fcdd543
terbeche <termus96@gmail.com>
2023-04-12 05:49:33 +0100

    Add session expiration after 1 year to stay login after closing the browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,10 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
-# skip_before_action :require_user!, only: [:sign_in]
-
   include Passwordless::ControllerHelpers
 
   helper_method :current_user
+
 
   private
 
